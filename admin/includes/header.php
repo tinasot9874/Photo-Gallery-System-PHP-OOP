192276c9bd875ccd1dec60295e75a7a6f072dea1
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-05-28 10:11:50 +0700

    organize admin structure

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -18,7 +18,7 @@
     <link href="css/sb-admin.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/ce8f710565.js" crossorigin="anonymous"></script>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
---
+++ admin/includes/side_nav.php
@@ -0,0 +1,40 @@
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <li>
+            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+        </li>
+        <li>
+            <a href="users.php"><i class="fas fa-fw fa-users"></i> Users</a>
+        </li>
+
+        <li>
+            <a href="comments.php"><i class="fas fa-comments"></i> Comments</a>
+        </li>
+        <li>
+            <a href="photos.php"><i class="fas fa-images"></i> Photos</a>
+        </li>
+        <li>
+            <a href="upload.php"><i class="fas fa-upload"></i> Uploads</a>
+        </li>
+<!--        <li>-->
+<!--            <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Bootstrap Grid</a>-->
+<!--        </li>-->
+<!--        <li>-->
+<!--            <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>-->
+<!--            <ul id="demo" class="collapse">-->
+<!--                <li>-->
+<!--                    <a href="#">Dropdown Item</a>-->
+<!--                </li>-->
+<!--                <li>-->
+<!--                    <a href="#">Dropdown Item</a>-->
+<!--                </li>-->
+<!--            </ul>-->
+<!--        </li>-->
+<!--        <li class="active">-->
+<!--            <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Blank Page</a>-->
+<!--        </li>-->
+<!--        <li>-->
+<!--            <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> RTL Dashboard</a>-->
+<!--        </li>-->
+    </ul>
+</div>
