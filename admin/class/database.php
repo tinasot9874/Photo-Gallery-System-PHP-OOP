2a7979cb5fecb14e3ad0b17e745a4cc6bdcef9e7
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-05-28 11:37:38 +0700

    create database class

---
+++ admin/class/database.php
@@ -0,0 +1,43 @@
+<?php
+require_once "core/config.php";
+
+class Database{
+
+    public $conn;
+
+    function __construct()
+    {
+        // Create connection
+        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASS,DB_NAME);
+        // Check connection
+        if ($this->conn->connect_error) {
+            die("Connection failed: " .$this->conn->connect_error);
+        }
+    }
+
+    public function query($sql){
+        $result = $this->conn->query($sql);
+        $this->confirm_query($result);
+        return $result;
+    }
+
+    private function confirm_query($result){
+        if (!$result){
+            die("Something wrong with query string! </br>" . $this->conn->error);
+        }
+    }
+
+    public function escape_string($string){
+       $escape_string = $this->conn->real_escape_string($this->conn,$string);
+       return $escape_string;
+    }
+
+    public function the_insert_id(){
+        return $this->conn->insert_id;
+    }
+
+
+
+}
+
+$database = new Database();
