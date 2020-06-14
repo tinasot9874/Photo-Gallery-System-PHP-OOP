ad542eb79feb4399f4b85dedd5b87e8c3b7b6332
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-06-14 19:36:09 +0700

    update config.php    + define site root

--- admin/core/config.php
+++ admin/core/config.php
@@ -5,4 +5,12 @@ define("DB_HOST","localhost");
 define("DB_USER","root");
 define("DB_PASS","");
 define("DB_NAME","gallery_db");
+
+//
+$root_path = substr(__DIR__, 0, -11);
+define('DS', DIRECTORY_SEPARATOR);
+define('SITE_ROOT', $root_path);
+echo SITE_ROOT;
+
+
 ?>
