12095800108b395ce2383682bfb3fb79b958bc67
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-06-17 02:16:39 +0700

    update edit.php    + add code to display data and update

--- admin/edit.php
+++ admin/edit.php
@@ -13,7 +13,21 @@
             <!-- Page Heading -->
             <?php
             if (isset($_GET['edit_photo'])) {
-                include("includes/photo_edit.php");
+                $photo = Photo::find_by_id($_GET['edit_photo']);
+                if ($photo){
+                    $cate = Category::find_by_id($photo->categories_id);
+                    if (isset($_POST['update_photo'])){
+                        $photo->title = $_POST['title'];
+                        $photo->categories_id = $_POST['cate'];
+                        $photo->save();
+                        redirect($_SERVER['REQUEST_URI']);
+                    }
+                    include("includes/photo_edit.php");
+                }else{
+                    redirect('404.html');
+                }
+            }else{
+                redirect('photos.php');
             }
             ?>
 
