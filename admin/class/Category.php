daf51f18a7e540feed87b411060a7b31996c6bc1
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-06-20 01:42:38 +0700

    update class Category.php     + add cate_slug table field

--- admin/class/Category.php
+++ admin/class/Category.php
@@ -4,7 +4,8 @@
 class Category extends Db_object
 {
     protected static $db_table = "categories";
-    protected static $db_table_fields = array('category_name');
+    protected static $db_table_fields = array('category_name', 'cate_slug');
     public $id;
     public $category_name;
+    public $cate_slug;
 }
