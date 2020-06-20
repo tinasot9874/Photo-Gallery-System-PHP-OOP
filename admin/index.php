bc62d7efa4ec1cd2f31abd78f0ca562343babbe0
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-06-21 02:43:52 +0700

    fix

--- admin/index.php
+++ admin/index.php
@@ -14,15 +14,11 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Blank Page
-                            <small>Subheading</small>
+                            Dashboard Page
                         </h1>
                         <ol class="breadcrumb">
                             <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
+                                <i class="fa fa-dashboard"></i>  <a href="#">Dashboard</a>
                             </li>
                         </ol>
                     </div>
