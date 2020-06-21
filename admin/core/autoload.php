b091745f789df0c1ff1480d684a91b1f7e655c2c
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-06-21 23:20:28 +0700

    update autoload.php    + fix bugs

--- admin/core/autoload.php
+++ admin/core/autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 function autoload($class){
-    $class = strtolower($class);
+
     $the_path = "class/{$class}.php";
     if (is_file($the_path) && !class_exists($class)){
         require_once ($the_path);
