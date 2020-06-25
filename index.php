677a2adfee9ee3ea73d942b304446f78303bf915
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-06-25 14:19:15 +0700

    update index.php    + display image

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+require_once ("admin/core/init.php");
+$photo = Photo::find_all();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -59,8 +64,9 @@
 			<section class="ftco-section-2">
 				<div class="photograhy">
 					<div class="row no-gutters">
+                        <?php foreach ($photo as $value): ?>
 						<div class="col-md-4 ftco-animate">
-							<a href="images/image_1.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_1.jpg);">
+							<a href="<?php echo "admin".DS.$value->picture_path(); ?>" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(admin/images/thumbnail/<?php echo $value->filename; ?>); object-fit: fill;">
 								<div class="overlay"></div>
 								<div class="text text-center">
 									<h3>Work 01</h3>
@@ -68,105 +74,8 @@
 								</div>
 							</a>
 						</div>
-						<div class="col-md-4 ftco-animate">
-							<a href="images/image_2.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_2.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 02</h3>
-									<span class="tag">Nature</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-4 ftco-animate">
-							<a href="images/image_3.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_3.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 03</h3>
-									<span class="tag">Fashion</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-4 ftco-animate">
-							<a href="images/image_4.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_4.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 04</h3>
-									<span class="tag">Travel</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-4 ftco-animate">
-							<a href="images/image_5.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_5.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 05</h3>
-									<span class="tag">Travel</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-4 ftco-animate">
-							<a href="images/image_6.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_6.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 06</h3>
-									<span class="tag">Travel</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-4 ftco-animate">
-							<a href="images/image_7.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_7.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 07</h3>
-									<span class="tag">Fashion, Model</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-4 ftco-animate">
-							<a href="images/image_8.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_8.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 08</h3>
-									<span class="tag">Nature</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-4 ftco-animate">
-							<a href="images/image_9.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_9.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 09</h3>
-									<span class="tag">Technology</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-4 ftco-animate">
-							<a href="images/image_10.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_10.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 10</h3>
-									<span class="tag">Model</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-4 ftco-animate">
-							<a href="images/image_11.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_11.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 11</h3>
-									<span class="tag">Fashion</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-4 ftco-animate">
-							<a href="images/image_12.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_12.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 12</h3>
-									<span class="tag">Photography</span>
-								</div>
-							</a>
-						</div>
+                        <?php endforeach; ?>
+
 					</div>
 				</div>
 			</section>
