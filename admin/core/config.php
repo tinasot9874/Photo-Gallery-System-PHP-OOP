c57cdf2ed9ba4c0f912dec72c57d85e521d234a6
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-06-25 14:18:10 +0700

    update config.php    + remove define path root

--- admin/core/config.php
+++ admin/core/config.php
@@ -6,11 +6,7 @@ define("DB_USER","root");
 define("DB_PASS","");
 define("DB_NAME","gallery_db");
 
-//
-$root_path = substr(__DIR__, 0, -11);
-define('DS', DIRECTORY_SEPARATOR);
-define('SITE_ROOT', $root_path);
-define('INCLUDES_PATH', SITE_ROOT.DS.'admin'.DS.'uploads');
+
 
 
 ?>
