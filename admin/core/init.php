420c6f23456e15828458d7d0a503c2c1df6148ff
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-06-25 14:18:45 +0700

    update init.php    + fix define path

--- admin/core/init.php
+++ admin/core/init.php
@@ -1,7 +1,16 @@
 <?php
+// define root_dir path
+$root_path = substr(__DIR__, 0, -11);
 
-require_once("class/Database.php");
-require_once("class/Session.php");
-require_once("autoload.php");
-require_once("helper.php");
-?>
+define('DS', DIRECTORY_SEPARATOR);
+define('SITE_ROOT', $root_path);
+
+define('PATH_CLASS', SITE_ROOT.DS.'admin'.DS.'class');
+define('PATH_CORE', SITE_ROOT.DS.'admin'.DS.'core');
+
+require_once (PATH_CORE.DS."config.php");
+require_once(PATH_CLASS.DS."Database.php");
+require_once(PATH_CLASS.DS."Session.php");
+require_once(PATH_CORE.DS."autoload.php");
+require_once(PATH_CORE.DS."helper.php");
+
